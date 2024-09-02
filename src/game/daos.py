de83d75141dbdc58d2321b481eef8aeacc391407
Kevin <kevin.rivers14832@ku.edu>
2024-09-02 15:44:34 -0500

    seperate concerns

---
+++ src/game/daos.py
@@ -0,0 +1,34 @@
+import logging
+import uuid
+from app import connections
+
+def init_move(state: object):
+    try:
+        print('hi')
+        with connections.cursor() as db:
+            db.execute("""select state
+                       from game_session
+                       where state = $s""", (state))
+    except Exception as e:
+        logging.error('Unable to process init move', e)
+
+
+
+def make_move(game_id: int):
+    with connections.cursor() as db:
+        db.execute("""
+        select game_expiration, game_phase, game_state
+                   from game session
+                   where game_id = %s
+"""), (game_id)
+
+def forfeit_game(session_id: uuid):
+    try:
+        with connections.cursor() as db:
+            db.execute("""
+                delete from game_session where session_id = %s;
+            """, (session_id,))
+
+        logging.info(f"Safely terminated session {session_id}")
+    except Exception as e:
+        logging.error('error on end_session', e)
--- src/lobby/daos.py
+++ src/lobby/daos.py
@@ -55,34 +55,3 @@ def game_status(game_id: int):
                    FROM game_session
             where game_id = %s
         """), (game_id)
-
-def init_move(state: object):
-    try:
-        print('hi')
-        with connections.cursor() as db:
-            db.execute("""select state
-                       from game_session
-                       where state = $s""", (state))
-    except Exception as e:
-        logging.error('Unable to process init move', e)
-
-
-
-def make_move(game_id: int):
-    with connections.cursor() as db:
-        db.execute("""
-        select game_expiration, game_phase, game_state
-                   from game session
-                   where game_id = %s
-"""), (game_id)
-
-def forfeit_game(session_id: uuid):
-    try:
-        with connections.cursor() as db:
-            db.execute("""
-                delete from game_session where session_id = %s;
-            """, (session_id,))
-
-        logging.info(f"Safely terminated session {session_id}")
-    except Exception as e:
-        logging.error('error on end_session', e)
