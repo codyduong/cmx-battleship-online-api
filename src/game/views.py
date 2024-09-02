8d6335ef98b1766e5a103ef2936385a65cf1d9de
Kevin <kevin.rivers14832@ku.edu>
2024-09-02 16:33:54 -0500

    Changing logic to delete staticmethod

--- src/game/views.py
+++ src/game/views.py
@@ -15,18 +15,17 @@ class GameMovesView(SessionView):
 
     @staticmethod
     def post(request: Request) -> Response:
-        #Need to edit logic?
+        #Need to edit logic
         current_player_id = request.user.player_id
         requested_player_id = request.data.get('player_id')
         game_dao.create_game_request(current_player_id, requested_player_id)
         return Response(status = 204)
     
     @staticmethod
-    #Need to edit logic?
     def delete(request: Request) -> Response:
         session_id = request.user.session_id
         if session_id is not None:
-            game_dao.end_session(session_id)
+            game_dao.forfeit_game(session_id)
         return Response(status=204)
     
     
