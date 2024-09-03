0fbafe183f0ea9b41cf7b92b15d6dca8fc270c8d
morriswa <william@morriswa.org>
2024-09-03 14:48:10 -0500

    hotfix: allowed hosts 2

--- src/app/prod.py
+++ src/app/prod.py
@@ -14,7 +14,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
     'https://www.morriswa.org',
 ]
-ALLOWED_HOSTS = ['cmx-battleship-online-api.f2iq9gqbr52r0.us-east-2.cs.amazonlightsail.com', subprocess.check_output(['hostname', '-i'])]
+ALLOWED_HOSTS = ['cmx-battleship-online-api.f2iq9gqbr52r0.us-east-2.cs.amazonlightsail.com', '[::1]']
 CORS_ALLOW_METHODS = (
     "GET",
     "POST",
