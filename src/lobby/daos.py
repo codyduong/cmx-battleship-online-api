d55bc7c4c3f5c946fc867af3594fa0c903e5d8d6
morriswa <william@morriswa.org>
2024-09-04 07:24:44 -0500

    utilize django db tools vs anon blocks psql for better error handling

--- src/lobby/daos.py
+++ src/lobby/daos.py
@@ -54,71 +54,71 @@ def get_game_requests(player_id: str) -> list[GameRequest]:
         
 def create_game_request(player_invite_from: str, player_invite_to: str):
 
+    if player_invite_from == player_invite_to:
+        raise BadRequestException('cannot request to match yourself')
+
     with connections.cursor() as db:
+        db.execute("select 1 from game_request where player_invite_from = %s and player_invite_to = %s",
+                   (player_invite_from, player_invite_to,))
+        result = db.fetchone()
+        if result is not None: return
+
         db.execute("""
-            do $$
-            declare
-                var_player_invite_from char(4) := %s;
-                var_player_invite_to char(4) := %s;
-            begin
-                insert into game_request (player_invite_from, player_invite_to) 
-                VALUES (var_player_invite_from, var_player_invite_to);
-                
-                delete from game_session
-                where 
-                    player_one_id = var_player_invite_from 
-                    or player_two_id = var_player_invite_from;
-            end$$;
+            insert into game_request (player_invite_from, player_invite_to) 
+            VALUES (%s, %s);
         """, (player_invite_from, player_invite_to,))
+
+        db.execute("""
+            delete from game_session
+            where 
+                player_one_id = %s 
+                or player_two_id = %s;
+        """, (player_invite_from, player_invite_from,))
 
 def accept_match_request(player_id: int, game_request_id: int):
 
     with connections.cursor() as db:
+
+        new_game_uuid = uuid.uuid4()
+
         db.execute("""
-            DO $$
-            DECLARE
-                -- inputs
-                param_game_request_id bigint := %s;
-                param_current_player_id char(4) := %s;
-                param_new_game_uuid uuid := %s;
-                
-                -- variables
-                player_invite_from char(4);
-                player_invite_to char(4);
-                num_ships char;
-            BEGIN
-                SELECT 
-                    gr.player_invite_from,
-                    gr.player_invite_to,
-                    us.num_ships
-                INTO 
-                    player_invite_from,
-                    player_invite_to,
-                    num_ships
-                FROM game_request gr
-                left join user_session us 
-                    on gr.player_invite_from = us.player_id
-                WHERE 
-                    gr.game_request_id = param_game_request_id
-                    and gr.player_invite_from = param_current_player_id 
-                    or gr.player_invite_to = param_current_player_id;
-                    
-                insert into game_session
-                    (game_id,
-                    player_one_id, player_two_id,
-                     active_turn, num_ships, 
-                     game_phase, game_state)
-                values
-                    (param_new_game_uuid,
-                    player_invite_to, player_invite_from,
-                    'p1', num_ships,
-                    'selct','{}');
-                    
-                delete from game_request gr
-                where gr.player_invite_from = param_current_player_id
-                or gr.player_invite_to = param_current_player_id;
-            END$$;   
-        """, (game_request_id, player_id, uuid.uuid4()))
+            SELECT 
+                gr.player_invite_from,
+                gr.player_invite_to,
+                us.num_ships
+            FROM game_request gr
+            left join user_session us 
+                on gr.player_invite_from = us.player_id
+            WHERE 
+                gr.game_request_id = %s
+                and gr.player_invite_from = %s 
+                or gr.player_invite_to = %s;
+        """, (game_request_id, player_id, player_id,))
+        result = db.fetchone()
+        if result is None: raise BadRequestException('no such request')
+        
+        player_one_id = result.get('player_invite_to')
+        player_two_id = result.get('player_invite_from')
+        num_ships = result.get('num_ships')
+        
+        db.execute("""
+            insert into game_session
+                (game_id,
+                player_one_id, player_two_id,
+                 active_turn, num_ships, 
+                 game_phase, game_state)
+            values
+                (%s,
+                 %s, %s,
+                'p1', %s,
+                'selct','{}');
+        """, (new_game_uuid, player_one_id, player_two_id, num_ships))
+
+        db.execute("""             
+            delete from game_request gr
+            where gr.player_invite_from = %s
+            or gr.player_invite_to = %s;
+        """, (player_id, player_id))
 
 def request_random_match():
     raise NotImplementedError('please implement me!')
