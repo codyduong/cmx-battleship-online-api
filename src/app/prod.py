c3c1f3c7155ef2e58740d7bf2817a368870ecf9a
morriswa <william@morriswa.org>
2024-09-04 21:43:03 -0500

    try nginx proxy aws 5

--- src/app/prod.py
+++ src/app/prod.py
@@ -17,7 +17,11 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
     'https://www.morriswa.org',
 ]
-ALLOWED_HOSTS = ['cmx-battleship-online-api.f2iq9gqbr52r0.us-east-2.cs.amazonlightsail.com', gethostbyname(gethostname())]
+ALLOWED_HOSTS = [
+    'cmx-battleship-online-api.f2iq9gqbr52r0.us-east-2.cs.amazonlightsail.com',
+    gethostbyname('cmx-battleship-online-api.f2iq9gqbr52r0.us-east-2.cs.amazonlightsail.com'),
+    gethostbyname(gethostname())
+]
 CORS_ALLOW_METHODS = (
     "GET",
     "POST",
